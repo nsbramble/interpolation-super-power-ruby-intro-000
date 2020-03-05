4710e4af225caaa659753dec594e506f047f16ff
New York  City Girl <nsbramble@gmail.com>
2020-03-05 23:12:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def 
-  display_rainbow(colors)
+  display_rainbow(color)
 puts "R: #{color0}", "O: #{color1}", "Y: #{color2}", "G: #{color3}", "B: #{color4}", "I: #{color5}", "V: #{color6}"
 end  
 
