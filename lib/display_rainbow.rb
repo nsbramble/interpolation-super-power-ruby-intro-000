6e68411323fdd89d60ab9a870954336a1e0baae1
New York  City Girl <nsbramble@gmail.com>
2020-03-05 23:06:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,6 @@
 # Write your #display_rainbow method here
-def display_rainbow(color0, color1, color2, color3, color4, color5, color6)
+def 
+  display_rainbow([color0, color1, color2, color3, color4, color5, color6])
 puts "R: #{color0}", "O: #{color1}", "Y: #{color2}", "G: #{color3}", "B: #{color4}", "I: #{color5}", "V: #{color6}"
 end  
 
